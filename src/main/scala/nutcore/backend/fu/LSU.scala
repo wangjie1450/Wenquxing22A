a2e7359d8232c111894335ef833b8fd88c0d1096
Dyingman2019 <dyingmail@163.com>
2022-04-10 22:26:35 +0800

    Remove duplicate instructions

--- src/main/scala/nutcore/backend/fu/LSU.scala
+++ src/main/scala/nutcore/backend/fu/LSU.scala
@@ -39,9 +39,9 @@ object LSUOpType { //TODO: refactor LSU fuop
   def sd   = "b0001011".U
   
   def nst  = "b0111011".U
-  def sst  = "b1001011".U
+  //def sst  = "b1001011".U
   def nld  = "b1011011".U
-  def sld  = "b1101011".U
+  //def sld  = "b1101011".U
 
   def lr      = "b0100000".U
   def sc      = "b0100001".U
@@ -57,8 +57,8 @@ object LSUOpType { //TODO: refactor LSU fuop
   
   def isAdd(func: UInt) = func(6)
   def isAtom(func: UInt): Bool = func(5)
-  def isStore(func: UInt): Bool = func(3) | func === nst | func === sst
-  def isLoad(func: UInt): Bool = !isStore(func) & !isAtom(func) | func === nld | func === sld
+  def isStore(func: UInt): Bool = func(3) | func === nst
+  def isLoad(func: UInt): Bool = !isStore(func) & !isAtom(func) | func === nld
   def isLR(func: UInt): Bool = func === lr
   def isSC(func: UInt): Bool = func === sc
   def isAMO(func: UInt): Bool = isAtom(func) && !isLR(func) && !isSC(func)
